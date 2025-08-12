c0a588340bd00b56c1b08d72e5b578b2538e7e72
Aliaksandr Ivanou <aleksivanov.me@gmail.com>
2025-08-12 20:01:47 +0200

    fix: pass for workers

---
+++ init-pgpass.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+PGPASSFILE="/var/lib/postgresql/.pgpass"
+echo "citus-worker-1:5432:${POSTGRES_DB}:${POSTGRES_USER}:${POSTGRES_PASSWORD}" > "$PGPASSFILE"
+echo "citus-worker-2:5432:${POSTGRES_DB}:${POSTGRES_USER}:${POSTGRES_PASSWORD}" >> "$PGPASSFILE"
+
+chmod 600 "$PGPASSFILE"
+chown postgres:postgres "$PGPASSFILE"
