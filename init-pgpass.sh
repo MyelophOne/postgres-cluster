4202c0ed6cde8d99ad70393d360c7370f4807188
Aliaksandr Ivanou <aleksivanov.me@gmail.com>
2025-08-16 13:01:02 +0200

    fix: try

--- init-pgpass.sh
+++ init-pgpass.sh
@@ -8,8 +8,8 @@ if [ "$ROLE" = "master" ]; then
 	echo "Configuring Citus master..."
 
 	cat > "$PGPASSFILE" <<EOF
-citus-worker-1:5432:${POSTGRES_DB}:${POSTGRES_USER}:${POSTGRES_PASSWORD}
-citus-worker-2:5432:${POSTGRES_DB}:${POSTGRES_USER}:${POSTGRES_PASSWORD}
+citus-worker-1:5432:${POSTGRES_DB}:*:${POSTGRES_PASSWORD}
+citus-worker-2:5432:${POSTGRES_DB}:*:${POSTGRES_PASSWORD}
 EOF
 	chown postgres:postgres "$PGPASSFILE"
 	chmod 600 "$PGPASSFILE"
